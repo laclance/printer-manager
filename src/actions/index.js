90546f2b694e7a6bed7ff72b8b8af43f04a0df32
Lance Coe <laclance@gmail.com>
2019-09-18 14:04:59 +0200

    Validate ip address;

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,10 +6,12 @@ import {
     DELETE_PRINTER_FAILED, DELETE_PRINTER_REQUEST, DELETE_PRINTER_SUCCESS
 } from './types';
 
+const ip_address_regex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+
 export const fetchPrinters = () => (dispatch) => {
     dispatch({ type: FETCH_PRINTERS_REQUEST });
 
-    return apiRequest('printers').then(
+    apiRequest('printers').then(
         printers => {
             dispatch({ type: FETCH_PRINTERS_SUCCESS, printers });
         },
@@ -20,34 +22,45 @@ export const fetchPrinters = () => (dispatch) => {
 };
 
 export const addPrinter = (data) => (dispatch) => {
-    dispatch({ type: ADD_PRINTER_REQUEST });
-    return apiRequest('printers', 'POST', data).then(
-        printer => {
-            dispatch({ type: ADD_PRINTER_SUCCESS, printer });
-        },
-        error => {
-            dispatch({ type: ADD_PRINTER_FAILED, error });
-        }
-    );
+    if (!ip_address_regex.test(data.ip_address)) {
+        dispatch({ type: ADD_PRINTER_FAILED, error: 'Invalid IP Address' });
+    }
+    else {
+        dispatch({ type: ADD_PRINTER_REQUEST });
+
+        apiRequest('printers', 'POST', data).then(
+            printer => {
+                dispatch({ type: ADD_PRINTER_SUCCESS, printer });
+            },
+            error => {
+                dispatch({ type: ADD_PRINTER_FAILED, error });
+            }
+        );
+    }
 };
 
 export const updatePrinter = (data) => (dispatch) => {
-    dispatch({ type: UPDATE_PRINTER_REQUEST });
+    if (!ip_address_regex.test(data.ip_address)) {
+        dispatch({ type: ADD_PRINTER_FAILED, error: 'Invalid IP Address' });
+    }
+    else {
+        dispatch({ type: UPDATE_PRINTER_REQUEST });
 
-    return apiRequest(`printers/${data.id}`, 'PUT', data).then(
-        printer => {
-            dispatch({ type: UPDATE_PRINTER_SUCCESS, printer });
-        },
-        error => {
-            dispatch({ type: UPDATE_PRINTER_FAILED, error });
-        }
-    );
+        apiRequest(`printers/${data.id}`, 'PUT', data).then(
+            printer => {
+                dispatch({ type: UPDATE_PRINTER_SUCCESS, printer });
+            },
+            error => {
+                dispatch({ type: UPDATE_PRINTER_FAILED, error });
+            }
+        );
+    }
 };
 
 export const deletePrinter = (id) => (dispatch) => {
     dispatch({ type: DELETE_PRINTER_REQUEST });
 
-    return apiRequest(`printers/${id}`, 'DELETE').then(
+    apiRequest(`printers/${id}`, 'DELETE').then(
         () => {
             dispatch({ type: DELETE_PRINTER_SUCCESS, id });
         },
